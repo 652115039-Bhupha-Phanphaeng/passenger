235ec5b27bed928881c775a7ceaf896c414011fe
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-08-09 08:16:07 +0700

    Initial commit (LAB03)

---
+++ src/views/NetworkErrorView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+const props = withDefaults(
+    defineProps<{
+        resource: string
+    }>(),
+    {
+        resource: 'event'
+    }
+)
+</script>
+
+<template>
+    <div class="networkError">
+        <h1>Uh-Oh!</h1>
+
+        <h3>It looks like you're experiencing some network issues, please take a breath and 
+            <a href="#" @click="$router.go(-1)"> Click here</a> to try again
+        </h3>
+    </div>
+</template>
---
+++ src/views/event/RegisterView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toRefs, defineProps } from 'vue'
+import { type Event } from '@/types'
+import { useRouter } from 'vue-router'
+
+const props = defineProps<{
+  event: Event
+  id: String
+}>()
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+  // If tthe registeration API call successful
+  // Push back to the event detajls view
+  router.push({ name: 'event-detail-view'})
+}
+</script>
+
+<template>
+  <p>Register event here</p>
+  <aside class="ai-send"></aside>
+  <button @click="register">Register</button>
+</template>
